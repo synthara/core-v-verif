6c67ec8fb44c12b8d492ac31e550f528da1a18f1
ValerioVenceslai-synthara <valeriovenceslai@synthara.ai>
2024-07-29 10:25:48 +0200

    Added hw_config folder

---
+++ hw_config/rvv_base_config.py
@@ -0,0 +1,20 @@
+rvv_base = {
+    "config_name": "rvv_base",
+
+    "rtl_files": [],
+
+    "rtl_incdirs": [],
+
+    "rtl_dependencies": [
+    ],
+
+    "tb_files": [],
+
+    "tb_incdirs": [],
+
+    "tb_dependencies": [], #TODO The TB is based on the old DSL library so it needs ddl as well
+
+    "verilog_wrapper": "",
+
+    "variants": [],
+}
