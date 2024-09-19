c8c751dafed9a947b01dcceaf998c6541c230241
MaurizioCapra-synthara <maurizio.capra@synthara.ai>
2024-09-19 11:05:52 +0200

    modified build.sh

--- build_run.sh
+++ build_run.sh
@@ -6,3 +6,4 @@ cd cv32e20/sim/uvmt && \
 make corev-dv CV_CORE=cv32e20 SIMULATOR=vcs && \
 make comp CV_CORE=cv32e20 SIMULATOR=vcs && \
 make test COREV=YES $TEST SIMULATOR=vcs GEN_START_INDEX=0 RUN_INDEX=0 USE_ISS=YES $GUI
+cd -
