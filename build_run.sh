d5843da612ae0746da2362fd2d765db0a661aa49
Francesco Pio De Malde <francesco.demalde@synthara.ai>
2024-09-19 18:03:23 +0200

    build_run.sh modified to remove past test results

--- build_run.sh
+++ build_run.sh
@@ -2,8 +2,19 @@
 TEST=${1:-"TEST=riscv_arithmetic_basic_test_0"}
 GUI=${2:-""}
 
-cd cv32e20/sim/uvmt && \
-make corev-dv CV_CORE=cv32e20 SIMULATOR=vcs && \
-make comp CV_CORE=cv32e20 SIMULATOR=vcs && \
+cd cv32e20/sim/uvmt
+make corev-dv CV_CORE=cv32e20 SIMULATOR=vcs 
+make comp CV_CORE=cv32e20 SIMULATOR=vcs 
+
+cd vcs_results/default
+test_name=$(echo "$TEST" | cut -d '=' -f 2)
+if [ -d "$test_name" ]; then
+    rm -r $test_name
+    echo "Removed previous test results: $test_name"
+else
+    echo "Test results not found."
+fi
+
+cd ../..
 make test COREV=YES $TEST SIMULATOR=vcs GEN_START_INDEX=0 RUN_INDEX=0 USE_ISS=YES $GUI
-cd -
+cd ../../../
