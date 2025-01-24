470b32ae616c091ab1647a8aa3852a5b44a83949
Michele Chilla <michele.chilla@synthara.ai>
2025-01-24 12:19:11 +0100

    Adding build_hw_config.py script to build .py hw_config

---
+++ hw_config/rvv_config.py
@@ -0,0 +1,68 @@
+rvv = {
+    "config_name": "rvv",
+
+    "rtl_files": [],
+
+    "rtl_incdirs": [],
+
+    "rtl_dependencies": [],
+
+    "behavioural_files": [],
+
+    "tb_files": [],
+
+    "tb_incdirs": [],
+
+    "tb_dependencies": [],
+
+    "verilog_wrapper": "",
+
+    "variants": [],
+
+}
+
+../core-v-cores/cv32e20/cv32e20_manifest.flist
+
+../cv32e20/tb/uvmt/uvmt_cv32e20.flist
+
+../cv32e20/env/uvme/uvme_cv32e20_pkg.flist
+../cv32e20/tb/uvmt/imperas_dv.flist
+../cv32e20/vendor_lib/verilab/svlib_pkg.flist
+../cv32e40p/env/uvme/uvme_cv32e40p_pkg.flist
+../cv32e40p/tb/uvmt/imperas_iss.flist
+../cv32e40p/tb/uvmt/uvmt_cv32e40p.flist
+../cv32e40p/vendor_lib/verilab/svlib_pkg.flist
+../cv32e40s/env/uvme/uvme_cv32e40s_pkg.flist
+../cv32e40s/tb/uvmt/imperas_iss.flist
+../cv32e40s/tb/uvmt/uvmt_cv32e40s.flist
+../cv32e40s/vendor_lib/verilab/svlib_pkg.flist
+../cv32e40x/env/uvme/uvme_cv32e40x_pkg.flist
+../cv32e40x/tb/uvmt/imperas_iss.flist
+../cv32e40x/tb/uvmt/uvmt_cv32e40x.flist
+../cv32e40x/vendor_lib/verilab/svlib_pkg.flist
+
+../lib/uvm_agents/uvma_axi/src/uvma_axi_pkg.flist
+../lib/uvm_agents/uvma_axi5/src/uvma_axi_pkg.flist
+../lib/uvm_agents/uvma_clknrst/uvma_clknrst_pkg.flist
+../lib/uvm_agents/uvma_core_cntrl/uvma_core_cntrl_pkg.flist
+../lib/uvm_agents/uvma_cvxif/src/uvma_cvxif_pkg.flist
+../lib/uvm_agents/uvma_debug/uvma_debug_pkg.flist
+../lib/uvm_agents/uvma_fencei/uvma_fencei_pkg.flist
+../lib/uvm_agents/uvma_interrupt/uvma_interrupt_pkg.flist
+../lib/uvm_agents/uvma_isacov/uvma_isacov_pkg.flist
+../lib/uvm_agents/uvma_obi_memory/src/uvma_obi_memory_pkg.flist
+../lib/uvm_agents/uvma_pma/src/uvma_pma_pkg.flist
+../lib/uvm_agents/uvma_reset/uvma_reset_pkg.flist
+../lib/uvm_agents/uvma_rvfi/uvma_rvfi_pkg.flist
+../lib/uvm_agents/uvma_rvvi/uvma_rvvi_pkg.flist
+../lib/uvm_agents/uvma_rvvi_ovpsim/uvma_rvvi_ovpsim_pkg.flist
+
+../lib/uvm_components/uvmc_rvfi_reference_model/uvmc_rvfi_reference_model_pkg.flist
+../lib/uvm_components/uvmc_rvfi_scoreboard/uvmc_rvfi_scoreboard_pkg.flist
+
+../lib/uvm_libs/uvml_hrtbt/uvml_hrtbt_pkg.flist
+../lib/uvm_libs/uvml_logs/uvml_logs_pkg.flist
+../lib/uvm_libs/uvml_mem/uvml_mem_pkg.flist
+../lib/uvm_libs/uvml_sb/uvml_sb_pkg.flist
+../lib/uvm_libs/uvml_trn/uvml_trn_pkg.flist
+
